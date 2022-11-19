f4dbe880de4414042a5c519771d7f0db8a2ef801
SK <sk@debian-BULLSEYE-live-builder-AMD64>
2022-11-19 16:32:17 -0500

    Define Post model for blog

--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,30 @@
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(max_lengt  = 256)
+    class Status(models.TextChoices):
+        DRAFT ='DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length =256)
+    slug = models.SlugField(max_length  =256)
+    authoor = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add= True)
     updated = models.DateTimeField(auto_now = True)
-
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default = Status.DRAFT)
+    
     class Meta:
         ordering= ['-publish']
-        index = [
+        indexes = [
             models.Index(fields=['-publish'])
         ]
     
