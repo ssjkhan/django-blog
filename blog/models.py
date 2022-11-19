2760d076db9f97e80c12e40c5b6952a91d6bb53c
SK <sk@debian-BULLSEYE-live-builder-AMD64>
2022-11-19 16:39:13 -0500

    Correct model field names

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
 
     title = models.CharField(max_length =256)
     slug = models.SlugField(max_length  =256)
-    authoor = models.ForeignKey(
+    author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='blog_posts')
