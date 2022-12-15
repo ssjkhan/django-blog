782f1c03c926e13aea01c97339718a0bd6c221fe
sk <jsckhan@outlook.com>
2022-12-14 20:20:44 -0500

    implement custom views for detailed posts and list of posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+# post views
+def post_list(req):
+    posts = Post.published.all()
+    data = {posts : posts}
+    return render(req, 'blog/post/list.html', data)
+
+
+def post_detail(req, id):
+    post = Post.published.get_object_or_404(id=id)
+
+    data = {'post' : post}
+    return render(req, 'blog/post/detail.html', data)
