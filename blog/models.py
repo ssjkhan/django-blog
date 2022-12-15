2611613716fccfbb468aff974477336b413c0b60
sk <jsckhan@outlook.com>
2022-12-14 20:16:10 -0500

    implement custom  manager- filter by published posts

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-# Create your models here.
+
+# Custom post manager
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+# post model
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT ='DF', 'Draft'
@@ -21,6 +27,9 @@ class Post(models.Model):
         max_length=2,
         choices=Status.choices,
         default = Status.DRAFT)
+
+    objects = models.Manager()
+    published = PublishedManager()
     
     class Meta:
         ordering= ['-publish']
@@ -30,5 +39,3 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
-
-    
