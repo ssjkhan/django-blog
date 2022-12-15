52da5d20d17cbd1e404ccde8a416b7434a6a50d8
sk <jsckhan@outlook.com>
2022-12-15 14:58:36 -0500

    implement aboslute url using models for routing to data defined views

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Custom post manager
 class PublishedManager(models.Manager):
@@ -14,7 +15,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length =256)
-    slug = models.SlugField(max_length  =256)
+    slug = models.SlugField(max_length  =256, unique_for_date='publish')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -39,3 +40,7 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    def get_absolute_url(self):
+        
+        return reverse("blog:post_detail", args=[self.pk])
+    
