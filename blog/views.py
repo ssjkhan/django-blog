daa73a889b1d2a7970f87ea1e334c9c50030735b
sk <jsckhan@outlook.com>
2022-12-17 12:14:46 -0500

    implement search using django search vector, and rank

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,11 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
-
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 # post views
 def post_list(req, tag_slug=None):
@@ -115,3 +115,26 @@ def post_comment(req, post_id):
     }
 
     return render(req, 'blog/post/comment.html', data)
+
+def post_search(req):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in req.GET:
+        form = SearchForm(req.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(
+                search=SearchVector('title', 'body'),
+                rank = SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
+
+    data = {
+        'form' : form,
+        'query' : query,
+        'results' : results
+    }
+    return render(req,'blog/post/search.html', data)
